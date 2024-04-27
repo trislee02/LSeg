068a460a301ced569b33665f25ba3a7949ea0cbe
Le Quang Tri <quangtri2002lk@gmail.com>
2024-04-27 16:10:59 +0700

    move image to cuda

--- test_demo.py
+++ test_demo.py
@@ -302,8 +302,8 @@ transform = transforms.Compose(
 image = transform(image).unsqueeze(0)
 img = image[0].permute(1,2,0)
 img = img * 0.5 + 0.5
-img = img.cuda()
-print("Image cuda: ", img.is_cuda)
+image = image.cuda()
+print("Image cuda: ", image.is_cuda)
 # plt.imshow(img)
 
 args.label_src = 'plant,grass,cat,stone,other'
